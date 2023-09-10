5d0e4c126d46a94d6fd3d40e9f376eb53a590ff6
root <root@DESKTOP-DHRCVPT>
2023-09-10 15:03:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Created and hosted an endpoint using any Expressjs
+The endpoint takes two GET request query parameters and returns specific information in JSON format.
+
+The informations include:
+Slack name
+Current day of the week
+Current UTC time (with validation of +/-2)
+Track
+The GitHub URL of the file being run
+The GitHub URL of the full source code.
+A  Status Code of Success
---
+++ stagOneTask.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+
+const app = express();
+const PORT = 3050;
+
+app.get('/api', (req, res) => {
+    //query parameters passed
+    const { slack_name, track } = req.query;
+
+    //get current day and utc time
+
+    const today = new Date().toLocaleDateString(('en-US'), {weekday: 'long'});
+    const utcDate = new Date().toISOString();
+
+    //the response object
+
+    const response = {
+        slack_name,
+        current_day: today,
+        utc_time: utcDate,
+        track,
+        github_file_url: '',
+        github_repo_url: '',
+        status_code: 200
+    }
+
+    res.json(response);
+})
+
+app.listen(PORT, ()=> 
+[
+    console.log(`you are currently on stageone listening on port ${PORT}`)
+])
